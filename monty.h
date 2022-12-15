b5a39aac9ca05ccc6924efc25eb36ca8de1c71c3
dannyEnagu <danielEnagu@gmail.com>
2022-12-15 21:11:11 +0000

    Daniel: missed monty header file

---
+++ monty.h
@@ -0,0 +1,59 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define  _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
+#include <ctype.h>
+
+/* Global variable that stores instruction operators*/
+extern char **monty_ip;
+
+/* Global macros */
+#define BUFFSIZE 3
+#define TOKEN_DELIMITER " \t\r\n\a"
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void malloc_failed(void);
+void parse_line(char *line);
+int arr_len(void);
+void _push(stack_t **stack, unsigned int line_number);
+void _print_stack(stack_t **stack, unsigned int line_number);
+void get_op_func(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
+
+#endif /* MONTY_H */
